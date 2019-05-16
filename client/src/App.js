fb70b767a72247032219cb95bbd794fe9ab5d250
kurtisp <kurtis.pace2011@gmail.com>
2019-05-16 16:03:56 -0600

    start of project

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <p>working</p>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ *      server.js
+ * 
+ */
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+  
+app.listen(PORT, function () { 
+
+    console.log("app listening on PORT: " + PORT);
+});
