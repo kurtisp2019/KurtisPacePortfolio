bfb1e878f23a768a31082328c422b26da0c94fb3
kurtisp <kurtis.pace2011@gmail.com>
2019-05-24 13:03:33 -0600

    adding gh-pages

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -43,20 +43,21 @@ class Header extends Component {
         </header>;
     }
 
+    // style={{ borderBottom: "1px solid white" }}
     getJSXHeaderCollapsedLinks() {
         
         return <header>
             <Navbar expand="lg" className = "mobileHeading">
-                <Navbar.Brand href="#home" style={{ marginRight: 0,color: "white" }}>Kurtis Pace Portfolio</Navbar.Brand>
+                <Navbar.Brand href="#home" style={{ color: "white", borderBottom: "1px solid white" }}>Kurtis Pace Portfolio</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" style = {{backgroundColor: "white"}}/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/education">Education</Nav.Link>
-                        <Nav.Link href="/workexperience">Work Experience</Nav.Link>
-                        <Nav.Link href="/skills">Skills</Nav.Link>
-                        <Nav.Link href="/resume">Resume</Nav.Link>
-                        <Nav.Link href="/contact">Contact</Nav.Link>
+                        <Nav.Link style={{ color: "white" }} href="/">Home</Nav.Link>
+                        <Nav.Link style={{ color: "white" }} href="/education">Education</Nav.Link>
+                        <Nav.Link style={{ color: "white" }} href="/workexperience">Work Experience</Nav.Link>
+                        <Nav.Link style={{ color: "white" }} href="/skills">Skills</Nav.Link>
+                        <Nav.Link style={{ color: "white" }} href="/resume">Resume</Nav.Link>
+                        <Nav.Link style={{ color: "white" }} href="/contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
