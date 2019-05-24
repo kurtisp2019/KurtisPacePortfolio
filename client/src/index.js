c2fbd65afc65d6e6760b19128aa5acc3574a9b2f
kurtisp <kurtis.pace2011@gmail.com>
2019-05-24 15:28:57 -0600

    g

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -10,4 +9,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
