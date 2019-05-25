486fc5736a5127498de773568ffd825cd8b69578
kurtisp <kurtis.pace2011@gmail.com>
2019-05-24 22:13:50 -0600

    working copy

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
+// reset all css before loading in the pages
+import "./styles/reset.css"
+
+// import pages
+import Home from "./pages/Home"
+import Education from "./pages/Education"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
+import WorkExperience from "./pages/WorkExperience"
+import Resume from "./pages/Resume"
+import Skills from "./pages/Skills"
+
+// import components
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import AddClientWidthAndHeight from "./pages/AddClientWidthAndHeight"
 
 function App() {
+
   return (
-    <p>working</p>
+    <>
+      <Router>
+
+        {/* Header */}
+        {<Header />}
+
+      {/* Body */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/education" component={Education} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/workexperience" component={WorkExperience} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/skills" component={Skills} />
+        </Switch>
+      </Router>
+
+      {/* Footer */}
+      <Footer />
+    </>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -9,3 +10,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
