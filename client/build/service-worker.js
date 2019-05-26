b39681802db832fe12ac57351eecf090963b0575
kurtisp <kurtis.pace2011@gmail.com>
2019-05-26 14:39:35 -0600

    exact path

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/KurtisPacePortfolio/precache-manifest.22c8545daf9c6913fd7d82350cc10461.js"
+  "/KurtisPacePortfolio/precache-manifest.8f4ab21cc3d094ba6d447b82902f3ddf.js"
 );
 
 self.addEventListener('message', (event) => {
