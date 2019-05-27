9b3a0b31dd377d7f950ed7b7d192ad7f9b3910a6
kurtisp <kurtis.pace2011@gmail.com>
2019-05-26 23:25:19 -0600

    Updates

--- precache-manifest.8a507ac53673a8600360eaecbc57df89.js
+++ precache-manifest.8a507ac53673a8600360eaecbc57df89.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a6042618cb808776a8af530ed43840a2",
+    "revision": "4be81e7e651e4b32bf1761d1e27cb772",
     "url": "/KurtisPacePortfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/KurtisPacePortfolio/static/css/2.df9ef6f4.chunk.css"
   },
   {
-    "revision": "78a7645333de9ec6f918",
+    "revision": "9665bdcbf4ea9e8ce7e1",
     "url": "/KurtisPacePortfolio/static/css/main.b85b56c4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/KurtisPacePortfolio/static/js/2.20420c77.chunk.js"
   },
   {
-    "revision": "78a7645333de9ec6f918",
-    "url": "/KurtisPacePortfolio/static/js/main.3c355b4b.chunk.js"
+    "revision": "9665bdcbf4ea9e8ce7e1",
+    "url": "/KurtisPacePortfolio/static/js/main.2040a331.chunk.js"
   },
   {
     "revision": "7aa51fd1649ffd4afb1e",
