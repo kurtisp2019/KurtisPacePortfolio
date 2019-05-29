16b14701c71a1360c8ac1f8cdea79e02c17f0c95
kurtisp <kurtis.pace2011@gmail.com>
2019-05-28 23:13:11 -0600

    made the contact page, no functionality yet

--- client/src/Views/Contact/ContactMe.js
+++ client/src/Views/Contact/ContactMe.js
@@ -13,21 +13,21 @@ function ContactMe() {
             <p style={{ textAlign: "center", fontWeight: "bolder", fontSize: "45px"}}>Contact Me</p>
             <hr style={{  marginTop: "20px", backgroundColor: "#285172", borderWidth: 3, width: "300px"}}></hr>
            
-            <form >
+            <form style={{ marginLeft: "5vw", marginRight: "5vw" }}>
                 <div style={{ paddingTop: "25px" }}>
-                    <p>Title: </p>
-                    <input type="text"></input>
+                    <p  >Title: </p>
+                    <input style={{ marginTop: "5px", height: "50px", width: "100%"}} type="text"></input>
                 </div>
                 <div style={{ paddingTop: "25px" }}>
                     <p>Email: </p>
-                    <input type="text"></input>
+                    <input style={{ marginTop: "5px", height: "50px", width: "100%"}} type="text"></input>
                 </div>
                 <div style={{ paddingTop: "25px" }}>
                     <p>Message: </p>
-                    <textarea></textarea>
+                    <textarea style={{ marginTop: "5px", width: "100%", height: "200px"}} ></textarea>
                 </div>
                 <br></br>
-                <button>Submit</button>
+                <button style={{ marginBottom: "25px", width: "200px", height: "50px", color: "white", backgroundColor: "#2096f3" }} >Submit</button>
             </form>
         </div>
     );
