6d9f85ecac23930c736b405639b3ba2e9374487e
kurtisp <kurtis.pace2011@gmail.com>
2019-06-01 15:13:41 -0600

    updated resume page

--- client/src/Views/Resume.js
+++ client/src/Views/Resume.js
@@ -9,7 +9,12 @@ import React from "react"
 function Resume() {
     
     return (
-        <p>Resume page </p>
+        <div style={{ textAlign: "center", color: "#285172", fontFamily: "'Montserrat', sans-serif", paddingTop: "40px", backgroundColor: "smokewhite" }}>
+        <p style={{ textAlign: "center", fontWeight: "bolder", fontSize: "45px" }}>Resume</p>
+        <hr style={{ marginTop: "20px", backgroundColor: "#285172", borderWidth: 3, width: "300px" }}></hr>
+
+            <p style= {{fontSize: "25px", paddingBottom: "75px"}}>Coming Soon!!!!!</p>
+        </div>
     );
 }
 
