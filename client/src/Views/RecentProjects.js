2cdb998c9e8b76608c2c61f3ef479063caa7ee5d
kurtisp <kurtis.pace2011@gmail.com>
2019-06-01 14:12:37 -0600

    added transition effect for recent projects page

--- client/src/Views/RecentProjects.js
+++ client/src/Views/RecentProjects.js
@@ -19,21 +19,127 @@ var Project = function (_imgURL, _title, _desc, _githubLink, _websiteLink) {
 
 class RecentProjects extends Component {
 
+
+    time = {
+        startTime: null
+    };
+
+    stateMachine = {
+        currentState: "",
+        //states: ["visible", "alphaOutNext", "alphaInNext", "alphaOutPrev", "alphaInPrev" ],
+
+        changeState: (_newState) => {
+            if (_newState === this.currentState)
+                return;
+
+            this.currentState = _newState;
+            // switch (this.currentState) {
+
+            //     case "visible":
+            //         break;
+            //     case "alphaOutNext":
+            //         break;
+            //     case "alphaInNext":
+            //         break;
+            //     case "alphaOutPrev":
+            //         break;
+            //     case "alphaInPrev":
+            //         break;
+            //     default:
+
+            // }
+        },
+
+        update: (_dt) => {
+            switch (this.currentState) {
+
+                case "visible":
+                    break;
+                case "alphaOutNext":
+                    this.setState({ opacity: this.state.opacity - this.state.transitionSpeed * _dt });
+                    if (this.state.opacity <= 0.0) {
+
+                        // cap the opacity
+                        this.setState({ opacity: 0.0 });
+
+                        // change the next state
+                        this.stateMachine.changeState("alphaInNext");
+
+                        // change the project to the next one
+                        this.setState({ m_curProject: this.state.m_curProject + 1 });
+
+                    }
+                    break;
+                case "alphaInNext":
+                    this.setState({ opacity: this.state.opacity + this.state.transitionSpeed * _dt });
+                    if (this.state.opacity >= 1.0) {
+
+                        // cap the opacity
+                        this.setState({ opacity: 1.0 });
+
+                        // change the next state
+                        this.stateMachine.changeState("visible");
+
+
+
+                    }
+                    break;
+                case "alphaOutPrev":
+                    this.setState({ opacity: this.state.opacity - this.state.transitionSpeed * _dt });
+                    if (this.state.opacity <= 0.0) {
+
+                        // cap the opacity
+                        this.setState({ opacity: 0.0 });
+
+                        // change the next state
+                        this.stateMachine.changeState("alphaInPrev");
+
+                        // change the project to the next one
+                        this.setState({ m_curProject: this.state.m_curProject - 1 });
+
+                    }
+                    break;
+
+                case "alphaInPrev":
+                    this.setState({ opacity: this.state.opacity + this.state.transitionSpeed * _dt });
+                    if (this.state.opacity >= 1.0) {
+
+                        // cap the opacity
+                        this.setState({ opacity: 1.0 });
+
+                        // change the next state
+                        this.stateMachine.changeState("visible");
+
+
+
+                    }
+                    break;
+
+                default:
+
+            }
+        }
+
+    };
+
     m_Projects = [];
 
     state = {
 
         opacity: 1.0,
+        transitionSpeed: 1.0,
         m_curProject: 0
 
     };
 
-
-
     componentWillMount() {
         this.addProjects();
 
-        //requestAnimationFrame(this.update);
+
+        // intitialize Time
+        this.time.startTime = new Date().getTime();
+
+        requestAnimationFrame(this.update);
     }
 
     addProjects() {
@@ -68,7 +174,16 @@ class RecentProjects extends Component {
 
     update = () => {
 
-        console.log("updating");
+
+        // find the time elapsed
+        var currentTime = new Date().getTime();
+        var dt = (currentTime - this.time.startTime) / 1000;
+        this.time.startTime = currentTime;
+
+        console.log("dt " + dt);
+
+        // update the state machine
+        this.stateMachine.update(dt);
 
         requestAnimationFrame(this.update);
 
@@ -77,37 +192,18 @@ class RecentProjects extends Component {
     nextProject = () => {
 
         if (this.state.m_curProject < this.m_Projects.length - 1)
-            this.setState({ m_curProject: this.state.m_curProject + 1 });
-        // if (this.state.m_curProject === this.m_Projects.length)
-        //     this.setState({ m_curProject: 0 });
+            this.stateMachine.changeState("alphaOutNext");
 
     }
     prevProject = () => {
         if (this.state.m_curProject > 0)
-            this.setState({ m_curProject: this.state.m_curProject - 1 });
-        // if (this.state.m_curProject < 0)
-        //     this.setState({ m_curProject: this.m_Projects.length - 1 });
+            this.stateMachine.changeState("alphaOutPrev");
 
-
-    }
-
-    getProjectPageArrowButtons() {
-        return ( <>
-         <div style={{ marginLeft: "200px" }}>
-                            <img onClick={this.prevProject} className="scaleImg" style={{ bottom: "-85px", width: "75px", height: "75px", position: "absolute" }} src={require("../resources/images/arrowBtn.png")} alt="arrowImg" />
-                        </div>
-                        {/* <img onClick={this.prevProject}  className="scaleImg" style={{ bottom: "-100px", marginLeft:"120px", width: "75px", height: "75px", position: "absolute" }} src={require("../resources/images/arrowBtn.png")} alt="arrowImg" /> */}
-                        <div style={{ marginLeft: "250px" }}>
-                        <img onClick={this.nextProject} className="flippedImg " style={{ bottom: "-85px", width: "75px", height: "75px", position: "absolute" }} src={require("../resources/images/arrowBtn.png")} alt="arrowImg" />
-                        </div>
-                        <label style={{ bottom: "-120px", marginLeft: "50px", position: "absolute" }}>Next</label>
-                      <label style={{ bottom: "-120px", marginLeft: "-100px", position: "absolute" }}>Previous</label>
-        </>);
     }
 
     getHomePageArrowButtons() {
         return (<>
-            <div style={{  height: "185px"}}>
+            <div style={{ height: "185px" }}>
                 <div style={{ positon: "relative" }}>
                     <img onClick={this.prevProject} className="scaleImg" style={{ marginTop: "25px", marginLeft: "110px", width: "75px", height: "75px", position: "absolute" }} src={require("../resources/images/arrowBtn.png")} alt="arrowImg" />
                     <img onClick={this.nextProject} className="flippedImg " style={{ marginTop: "25px", marginLeft: "110px", width: "75px", height: "75px", position: "absolute" }} src={require("../resources/images/arrowBtn.png")} alt="arrowImg" />
@@ -128,28 +224,29 @@ class RecentProjects extends Component {
             <>
 
 
-                <div style={{  opacity: this.state.opacity, textAlign: "center", color: "#285172", fontFamily: "'Montserrat', sans-serif", paddingTop: "40px", backgroundColor: "orange" }}>
+                <div style={{ textAlign: "center", color: "#285172", fontFamily: "'Montserrat', sans-serif", paddingTop: "40px", backgroundColor: "orange" }}>
                     <p style={{ textAlign: "center", fontWeight: "bolder", fontSize: "45px" }}>Recent Projects</p>
                     <hr style={{ marginTop: "20px", backgroundColor: "#285172", borderWidth: 3, width: "300px" }}></hr>
 
 
-                    <img style={{ width: "350px", height: "250px", paddingTop: "25px" }} src={this.m_Projects[this.state.m_curProject].imgURL} alt="screenshot" />
-                    <p key={this.m_Projects[this.state.m_curProject].title} style={{ paddingTop: "10px", fontSize: "35px" }}>{this.state.m_curProject + 1}. "{this.m_Projects[this.state.m_curProject].title}"</p>
-                    <hr style={{ marginTop: "20px", backgroundColor: "#285172", borderWidth: 3, width: "70px" }}></hr>
+                    <div style={{ opacity: this.state.opacity }}>
+                        <img style={{ width: "350px", height: "250px", paddingTop: "25px" }} src={this.m_Projects[this.state.m_curProject].imgURL} alt="screenshot" />
+                        <p key={this.m_Projects[this.state.m_curProject].title} style={{ paddingTop: "10px", fontSize: "35px" }}>{this.state.m_curProject + 1}. "{this.m_Projects[this.state.m_curProject].title}"</p>
+                        <hr style={{ marginTop: "20px", backgroundColor: "#285172", borderWidth: 3, width: "70px" }}></hr>
 
-                    <p style={{ fontSize: "25px", marginLeft: "5vw", marginRight: "5vw" }}> {this.m_Projects[this.state.m_curProject].description} </p>
-                    <br></br>
-                    <p>link to actual site:</p>
-                    <p style={{ fontSize: "20px", marginTop: "3px" }}><a href={this.m_Projects[this.state.m_curProject].githubLink}>{this.m_Projects[this.state.m_curProject].githubLink}</a></p>
-                    <br></br>
-                    <p >link to github code</p>
-                    <p style={{ fontSize: "20px", marginTop: "3px" }}><a href={this.m_Projects[this.state.m_curProject].websiteLink}>{this.m_Projects[this.state.m_curProject].websiteLink}</a></p>
-
+                        <p style={{ fontSize: "25px", marginLeft: "5vw", marginRight: "5vw" }}> {this.m_Projects[this.state.m_curProject].description} </p>
+                        <br></br>
+                        <p>link to actual site:</p>
+                        <p style={{ fontSize: "20px", marginTop: "3px" }}><a href={this.m_Projects[this.state.m_curProject].githubLink}>{this.m_Projects[this.state.m_curProject].githubLink}</a></p>
+                        <br></br>
+                        <p >link to github code</p>
+                        <p style={{ fontSize: "20px", marginTop: "3px" }}><a href={this.m_Projects[this.state.m_curProject].websiteLink}>{this.m_Projects[this.state.m_curProject].websiteLink}</a></p>
+                    </div>
                     <br></br>
                     <div>
                         {this.getHomePageArrowButtons()}
                     </div>
-            
+
                 </div>
 
             </>);
