a7c8cb56ac8f514f27944f8d02bfcc5921d32a0c
kurtisp <kurtis.pace2011@gmail.com>
2019-06-05 22:28:07 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/KurtisPacePortfolio/precache-manifest.1877cd838b6f2027cf13de0e1e325d97.js"
+  "/KurtisPacePortfolio/precache-manifest.e50ea2e83b8e0b3f8878b9328f99ac3a.js"
 );
 
 self.addEventListener('message', (event) => {
